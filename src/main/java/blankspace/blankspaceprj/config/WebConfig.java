e96425d8dd39fe1e1fcb449f8f0de864f42850b1
Shin chae won <tlscodnjs934@naver.com>
2023-04-11 01:14:17 +0900

    스웨거 적용

---
+++ src/main/java/blankspace/blankspaceprj/config/WebConfig.java
@@ -0,0 +1,16 @@
+package blankspace.blankspaceprj.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
