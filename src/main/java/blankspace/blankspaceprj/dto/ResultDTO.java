cb6a2baf1279c4193de4b182d52b2655a31225b3
Shin chae won <tlscodnjs934@naver.com>
2023-04-11 01:08:27 +0900

    상태 메세지 추가, 스웨거 설명 적용

--- src/main/java/blankspace/blankspaceprj/dto/ResultDTO.java
+++ src/main/java/blankspace/blankspaceprj/dto/ResultDTO.java
@@ -1,8 +1,16 @@
 package blankspace.blankspaceprj.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public class ResultDTO {
 
+    @ApiModelProperty(example = "상태코드")
     private String resultCode;
+
+    @ApiModelProperty(example = "상태 메세지")
+    private String resultMsg;
+
+    @ApiModelProperty(example = "데이터")
     private Object data;
 
     public String getResultCode() {
@@ -11,6 +19,14 @@ public class ResultDTO {
 
     public void setResultCode(String resultCode) {
         this.resultCode = resultCode;
+    }
+
+    public String getResultMsg() {
+        return resultMsg;
+    }
+
+    public void setResultMsg(String resultMsg) {
+        this.resultMsg = resultMsg;
     }
 
     public Object getData() {
