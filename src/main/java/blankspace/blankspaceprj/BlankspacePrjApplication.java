2202780b79fd4af5a410b712c58d1697c216f51f
Shin chae won <tlscodnjs934@naver.com>
2023-04-14 00:30:06 +0900

    스프링 시큐리티 로그인 비활성화

--- src/main/java/blankspace/blankspaceprj/BlankspacePrjApplication.java
+++ src/main/java/blankspace/blankspaceprj/BlankspacePrjApplication.java
@@ -2,8 +2,9 @@ package blankspace.blankspaceprj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BlankspacePrjApplication {
 
 	public static void main(String[] args) {
